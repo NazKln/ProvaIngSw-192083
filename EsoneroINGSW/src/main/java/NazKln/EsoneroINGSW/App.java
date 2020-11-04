6581d5a09ddc2ecddae7f8d791e0726454e1ef8c
nazar <nazikkln@gmail.com>
2020-11-04 12:51:18 +0100

    Utent Choose

--- EsoneroINGSW/src/main/java/NazKln/EsoneroINGSW/App.java
+++ EsoneroINGSW/src/main/java/NazKln/EsoneroINGSW/App.java
@@ -1,6 +1,7 @@
 package NazKln.EsoneroINGSW;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * Hello world!
@@ -11,25 +12,37 @@ public class App
     public static void main( String[] args )
     {
     	ArrayList lista=new ArrayList<Integer>();
-    	lista.add(5);
-    	lista.add(7);
-    	lista.add(2);
+    	ArrayList vedi= new ArrayList<Integer>();
+    	//lista.add(5);
+    	//lista.add(7);
+    	//lista.add(2);
         MyListUtil ml= new MyListUtil();
         
-        ArrayList vedi= new ArrayList<Integer>(ml.OrdinaCrescente(lista));
-        
+        Scanner sc= new Scanner(System.in);
+        int num=0;
+        int n=0;
+        System.out.println("Inserire il numero di elementi");
+        num=sc.nextInt();
+        System.out.println("Inserire i numeri");
+        for(int i=0; i<num; i++)
+        {
+        	n=sc.nextInt();
+        	lista.add(n);
+        }
+        System.out.println("Premi 0 per ordinare decrescente 1 crescente");
+        n=sc.nextInt(n);
+        switch(n)
+        {
+        case 0:
+        	vedi= new ArrayList<Integer>(ml.OrdinaDecrescente(lista));
+        	break;
+        case 1:
+        	vedi = new ArrayList<Integer>(ml.OrdinaCrescente(lista));
+        	break;
+        }
         for(int i=0; i<vedi.size(); i++)
         {
         	System.out.println(vedi.get(i));
         }
-        
-        ArrayList vediRew = new ArrayList<Integer>(ml.OrdinaDecrescente(lista));
-        
-        for(int i=0; i<vediRew.size(); i++)
-        {
-        	System.out.println(vediRew.get(i));
-        }
-        
-        
     }
 }
